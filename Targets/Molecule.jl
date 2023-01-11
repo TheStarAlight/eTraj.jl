42cff6893a3b89d0df767e43e3654ee5c9b5a41c
myzhu <myzhu@stu.ecnu.edu.cn>
2023-01-12 00:01:26 +0800

    feat(targets/Molecule) Added target Molecule with some basic properties (prop. for SFI are still TODOs).

--- Targets/Molecule.jl
+++ Targets/Molecule.jl
@@ -1,7 +1,37 @@
 
 "Represents a molecule."
 struct Molecule <: Target
-    "Atoms in the molecule, which are stored as a vector of (:AtomName, X, Y, Z)."
+    "Atoms in the molecule with their coordinates, which are stored as a vector of (:AtomName, X, Y, Z)."
     atoms;
+    "Total charge of the molecule (ion)."
+    molCharge;
+    "Name of the molecule."
     name::String;
+
+    """
+    Initializes a new instance of `Molecule` with given parameters.
+    # Parameters
+    - `atoms`       : Atoms in the molecule with their coordinates, which are stored as a vector of (:AtomName, X, Y, Z).
+    - `molCharge`   : Total charge of the molecule (ion) (optional, default 0).
+    - `name`        : Name of the molecule (optional).
+    """
+    function Molecule(atoms,molCharge=0,name::String="[NA]")
+        new(atoms,molCharge,name)
+    end
+    function Molecule(;atoms,molCharge=0,name::String="[NA]")
+        new(atoms,molCharge,name)
+    end
+end
+
+"Gets the atoms in the molecule with their coordinates."
+Atoms(mol::Molecule) = mol.atoms
+"Gets the total charge of the molecule (ion)."
+MolCharge(mol::Molecule) = mol.molCharge
+"Gets the name of the molecule."
+TargetName(mol::Molecule) = mol.name
+
+"Exports the given molecule's atom information to string as `MolecularCalculator`'s input."
+function exportMolAtomInfo(mol::Molecule)
+    atomToString(atom) = join([String(atom[1]),atom[2],atom[3],atom[4]], " ")
+    return join(map(atomToString, mol.atoms),";")
 end
--- Targets/Targets.jl
+++ Targets/Targets.jl
@@ -8,7 +8,10 @@ module Targets
 export Target, SAEAtomBase
 export SAEAtom, HydrogenLikeAtom
 export HAtom, He1pAtom, Li2pAtom, HeAtom, NeAtom, Ne1pAtom, Ne2pAtom, ArAtom, Ar1pAtom, Ar2pAtom, VAtom, NiAtom, KrAtom, Kr1pAtom, RbAtom, NbAtom, PdAtom, XeAtom, Xe1pAtom, TaAtom
-export IonPotential, AsympNuclCharge, TargetPotential, TargetForce, TrajectoryFunction, ADKRateExp
+export IonPotential, AsympNuclCharge, TargetName, TargetPotential, TargetForce, TrajectoryFunction, ADKRateExp
+
+export Molecule
+export Atoms, MolCharge, exportMolAtomInfo
 
 abstract type Target end
 abstract type SAEAtomBase <: Target end
