ab3f7ec4230bfb4b3c8f755fdbf1884736dd2341
myzhu <myzhu@stu.ecnu.edu.cn>
2023-05-03 22:03:02 +0800

    feat(SFA/rate_prefix) Added ExpJac prefix for SFA rate method.

--- src/SampleProviders/SFASampler.jl
+++ src/SampleProviders/SFASampler.jl
@@ -30,7 +30,7 @@ struct SFASampler <: ElectronSampleProvider
             return
         end
         # check IonRate prefix support.
-        if ! (rate_ionRatePrefix in [:ExpRate, :ExpPre])
+        if ! (rate_ionRatePrefix in [:ExpRate, :ExpPre, :ExpJac, :Full])
             error("[SFASampler] Undefined tunneling rate prefix [$rate_ionRatePrefix].")
             return
         end
@@ -75,19 +75,19 @@ function generateElectronBatch(sp::SFASampler, batchId::Int)
     init_thread = zeros(Float64, dim, nthreads(), pdNum*pzNum) # initial condition (support for multi-threading)
 
     "Saddle-point equation."
-    function saddlePointEquation(AxF,AyF,tr,ti,pd,pz)
+    function saddle_point_equation(AxF,AyF,tr,ti,kd,kz)
         Axt = AxF(tr+1im*ti)
         Ayt = AyF(tr+1im*ti)
-        px =  pd*imag(Ayt)/sqrt(imag(Axt)^2+imag(Ayt)^2) - real(Axt)
-        py = -pd*imag(Axt)/sqrt(imag(Axt)^2+imag(Ayt)^2) - real(Ayt)
-        return SVector(real(((px+Axt)^2+(py+Ayt)^2+pz^2)/2 + Ip))
+        px =  kd*imag(Ayt)/sqrt(imag(Axt)^2+imag(Ayt)^2) - real(Axt)
+        py = -kd*imag(Axt)/sqrt(imag(Axt)^2+imag(Ayt)^2) - real(Ayt)
+        return SVector(real(((px+Axt)^2+(py+Ayt)^2+kz^2)/2 + Ip))
     end
 
-    @threads for ipd in 1:pdNum
-        for ipz in 1:pzNum
-            pd, pz = sp.ss_pdSamples[ipd], sp.ss_pzSamples[ipz]
-            spe((ti,)) = saddlePointEquation(Ax,Ay,tr,ti,pd,pz)
-            ti_sol = nlsolve(spe, [asinh(ω/Ftr*sqrt(pd^2+pz^2+2Ip))/ω])
+    @threads for ikd in 1:pdNum
+        for ikz in 1:pzNum
+            kd, kz = sp.ss_pdSamples[ikd], sp.ss_pzSamples[ikz]
+            spe((ti,)) = saddle_point_equation(Ax,Ay,tr,ti,kd,kz)
+            ti_sol = nlsolve(spe, [asinh(ω/Ftr*sqrt(kd^2+kz^2+2Ip))/ω])
             ti = 0.0
             if converged(ti_sol) && (ti_sol.zero[1]>0)
                 ti = ti_sol.zero[1]
@@ -96,22 +96,50 @@ function generateElectronBatch(sp::SFASampler, batchId::Int)
                 continue
             end
             sample_count_thread[threadid()] += 1
-            ts = tr+1im*ti   # saddle point time
-            Axts, Ayts = Ax(ts), Ay(ts)
-            px =  pd*imag(Ayts)/sqrt(imag(Axts)^2+imag(Ayts)^2) - real(Axts)
-            py = -pd*imag(Axts)/sqrt(imag(Axts)^2+imag(Ayts)^2) - real(Ayts)
-            S  = quadgk(t->((px+Ax(t))^2+(py+Ay(t))^2+pz^2)/2 + Ip, ts, tr)[1] # Integrates ∂S/∂t from ts to tr.
+
+            @inline function px_(AxF,AyF,tr,ti,kd)
+                ts = tr+1im*ti   # saddle point time
+                Axts, Ayts = AxF(ts), AyF(ts)
+                return kd*imag(Ayts)/sqrt(imag(Axts)^2+imag(Ayts)^2) - real(Axts)
+            end
+            @inline function py_(AxF,AyF,tr,ti,kd)
+                ts = tr+1im*ti   # saddle point time
+                Axts, Ayts = AxF(ts), AyF(ts)
+                return -kd*imag(Axts)/sqrt(imag(Axts)^2+imag(Ayts)^2) - real(Ayts)
+            end
+            px = px_(Ax,Ay,tr,ti,kd)
+            py = py_(Ax,Ay,tr,ti,kd)
+            # px =  kd*imag(Ayts)/sqrt(imag(Axts)^2+imag(Ayts)^2) - real(Axts)
+            # py = -kd*imag(Axts)/sqrt(imag(Axts)^2+imag(Ayts)^2) - real(Ayts)
+            S  = quadgk(t->((px+Ax(t))^2+(py+Ay(t))^2+kz^2)/2 + Ip, tr+1im*ti, tr)[1] # Integrates ∂S/∂t from ts to tr.
             x0 = quadgk(ti->imag(Ax(tr+1im*ti)), 0.0, ti)[1]
             y0 = quadgk(ti->imag(Ay(tr+1im*ti)), 0.0, ti)[1]
             z0 = 0.0
-            px0 = px+Ax(tr)
-            py0 = py+Ay(tr)
-            pz0 = pz
+            kx0 = px+Ax(tr)
+            ky0 = py+Ay(tr)
+            kz0 = kz
             rate = exp(2*imag(S))
-            if sp.ionRatePrefix == :ExpPre
+            if sp.ionRatePrefix == :ExpPre || sp.ionRatePrefix == :Full
                 rate /= abs((px+Ax(tr+1im*ti))*Fx(tr+1im*ti)+(py+Ay(tr+1im*ti))*Fy(tr+1im*ti))^α
             end
-            init_thread[1:8,threadid(),sample_count_thread[threadid()]] = [x0,y0,z0,px0,py0,pz0,tr,rate]
+            if sp.ionRatePrefix == :ExpJac || sp.ionRatePrefix == :Full
+                dt = 0.01
+                dk = 0.01
+                spe_t_p_dt((ti,)) = saddle_point_equation(Ax,Ay,tr+dt,ti,kd,kz)
+                spe_t_m_dt((ti,)) = saddle_point_equation(Ax,Ay,tr-dt,ti,kd,kz)
+                spe_k_p_dk((ti,)) = saddle_point_equation(Ax,Ay,tr,ti,kd+dk,kz)
+                spe_k_m_dk((ti,)) = saddle_point_equation(Ax,Ay,tr,ti,kd-dk,kz)
+                ti_tp = nlsolve(spe_t_p_dt,[ti]).zero[1]
+                ti_tm = nlsolve(spe_t_m_dt,[ti]).zero[1]
+                ti_kp = nlsolve(spe_k_p_dk,[ti]).zero[1]
+                ti_km = nlsolve(spe_k_m_dk,[ti]).zero[1]
+                dpxdtr = px_(Ax,Ay,tr+dt,ti_tp,kd) - px_(Ax,Ay,tr-dt,ti_tm,kd)
+                dpydtr = py_(Ax,Ay,tr+dt,ti_tp,kd) - py_(Ax,Ay,tr-dt,ti_tm,kd)
+                dpxdkd = px_(Ax,Ay,tr,ti_kp,kd+dk) - px_(Ax,Ay,tr,ti_km,kd-dk)
+                dpydkd = py_(Ax,Ay,tr,ti_kp,kd+dk) - py_(Ax,Ay,tr,ti_km,kd-dk)
+                rate *= abs(dpxdtr*dpydkd-dpxdkd*dpydtr)/(4dt*dk)
+            end
+            init_thread[1:8,threadid(),sample_count_thread[threadid()]] = [x0,y0,z0,kx0,ky0,kz0,tr,rate]
             if sp.phaseMethod != :CTMC
                 init_thread[9,threadid(),sample_count_thread[threadid()]] = 0.0
             end
