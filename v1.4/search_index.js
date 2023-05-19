bae57b2591ef8ce4547f69b0cf76e47832427e37
Mingyu Zhu <myzhu@stu.ecnu.edu.cn>
2023-05-19 22:26:00 +0800

    update(gh-pages/test) Updated a test page.

---
+++ v1.4/search_index.js
@@ -0,0 +1,3 @@
+var documenterSearchIndex = {"docs":
+[{"location":"#SemiclassicalSFI.jl","page":"🎆SemiclassicalSFI.jl","title":"🎆SemiclassicalSFI.jl","text":"","category":"section"},{"location":"","page":"🎆SemiclassicalSFI.jl","title":"🎆SemiclassicalSFI.jl","text":"Implementation of semiclassical methods in strong field ionization of atoms and molecules.","category":"page"}]
+}
