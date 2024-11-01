0151b3468efa5e03bdc48818a9970d1ebad0ffa5
Mingyu Zhu <myzhu@stu.ecnu.edu.cn>
2024-11-02 00:49:27 +0800

    update(gh-pages/v1.0.0-rc3)

--- update_doc.jl
+++ update_doc.jl
@@ -1,6 +1,9 @@
 using Base.Filesystem
 
-version = "v1.0.0-rc2"    # the version number to update
+version = "v1.0.0-rc3"  # the version number to update
+symlink = "dev"         # the symlink to update
 
-rm("./$version/", recursive=true) # remove the current version
+rm("./$version/", force=true, recursive=true) # remove the current version
 cptree("./docs/build/", "./$version/")
+rm("./$symlink", force=true)
+run(`ln -sf $version $symlink`)
--- versions.js
+++ versions.js
@@ -1,5 +1,6 @@
 var DOC_VERSIONS = [
                     "dev",
-                    "v1.0.0-rc2"
+                    "v1.0.0-rc2",
+                    "v1.0.0-rc3"
                     ];
 var DOCUMENTER_STABLE = "dev";
