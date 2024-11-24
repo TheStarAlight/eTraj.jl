9f75c6d416ef7d6be2eab0c38680e2fbe17917f0
Mingyu Zhu <myzhu@stu.ecnu.edu.cn>
2024-11-24 22:08:34 +0800

    misc(remove_comment)

--- src/ElectronSamplers/SPASampler.jl
+++ src/ElectronSamplers/SPASampler.jl
@@ -238,7 +238,6 @@ function gen_electron_batch(sp::SPASampler, batch_id::Integer)
     end
     function solve_spe(tr,kd,kz)
         spe((ti,)) = saddle_point_equation(tr,ti,kd,kz)
-        # println("solve_spe: tr = $tr, kd = $kd, kz = $kz")
         ti_sol = nlsolve(spe, [asinh(ω/Ftr*sqrt(kd^2+kz^2+2Ip))/ω])
         ti = 0.0
         if converged(ti_sol) && (ti_sol.zero[1]>0)
