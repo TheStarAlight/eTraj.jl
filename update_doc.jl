815086e00bc16bf5f2d27271177b44638899effb
Mingyu Zhu <myzhu@stu.ecnu.edu.cn>
2025-01-20 12:25:32 +0800

    update(gh-pages/v1.0.0-rc5)

--- update_doc.jl
+++ update_doc.jl
@@ -1,6 +1,6 @@
 using Base.Filesystem
 
-version = "v1.0.0-rc3"  # the version number to update
+version = "v1.0.0-rc5"  # the version number to update
 symlink = "dev"         # the symlink to update
 
 rm("./$version/", force=true, recursive=true) # remove the current version
